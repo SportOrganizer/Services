d323b90e8a8bd6319f582de5b5c6bfdfccdf30bf
kristiankacinetz <kristian.kacinetz@gmail.com>
2017-05-25 20:00:19 +0200

    websocket commit

--- SportApp/Core/src/main/java/com/so/core/main/AppInitializer.java
+++ SportApp/Core/src/main/java/com/so/core/main/AppInitializer.java
@@ -16,11 +16,11 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class AppInitializer implements WebApplicationInitializer {
 
     public void onStartup(ServletContext servletContext) throws ServletException {
-        WebApplicationContext context = getContext();
-        servletContext.addListener(new ContextLoaderListener(context));
-        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/*");
+//        WebApplicationContext context = getContext();
+//        servletContext.addListener(new ContextLoaderListener(context));
+//        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
+//        dispatcher.setLoadOnStartup(1);
+//        dispatcher.addMapping("/*");
     }
 
     private AnnotationConfigWebApplicationContext getContext() {
